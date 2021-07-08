8e9f79771a0d6e79cbbe209650a1a6e96e379d55
Ryan <ryanpaynt@uncc.edu>
2021-07-08 18:55:29 -0400

    added a post route to api.js

--- routes/api.js
+++ routes/api.js
@@ -10,3 +10,12 @@ app.get("/api/workouts", (req,res) => {
     });
 });
 
+app.post("/api/workouts", (req,res) => {
+    Workout.create(body)
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => {
+        res.status(404).json(err)
+    });
+});
+
+
