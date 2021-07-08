df7505fa779d0fe8d94ffbbf2ca4e8dea4853af6
Ryan <ryanpaynt@uncc.edu>
2021-07-08 19:21:46 -0400

    added a path for the index html

--- routes/html.js
+++ routes/html.js
@@ -3,3 +3,6 @@ var path = require("path");
 app.get("/excersise", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
