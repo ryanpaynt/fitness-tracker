3c83bb6d1c9bfbdcd68abb34dd8774a826b74810
Ryan <ryanpaynt@uncc.edu>
2021-07-08 19:22:17 -0400

    added a path for the stats html

--- routes/html.js
+++ routes/html.js
@@ -6,3 +6,6 @@ app.get("/excersise", (req,res) => {
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+app.get("/stats", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
