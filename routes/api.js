794e1e78273349dd428a69789ad831b47420ae1a
Ryan <ryanpaynt@uncc.edu>
2021-07-08 21:44:38 -0400

    reversed export changes and changed app to router for all router methods

--- routes/api.js
+++ routes/api.js
@@ -1,51 +1,52 @@
+var router = require('express').Router();
 const Workout = require("../models/workout");
 
-module.exports = function (app) {
-    app.get("/api/workouts", (req, res) => {
-        Workout.find()
-            .then(data => {
-                res.json(data)
-            })
-            .catch(err => {
-                res.json(err)
-            });
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json(err)
     });
+});
 
-    app.post("/api/workouts", (req, res) => {
-        Workout.create({})
-            .then(dbWorkout => res.json(dbWorkout))
-            .catch(err => {
-                res.status(404).json(err)
-            });
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => {
+        res.status(404).json(err)
     });
+});
 
-    // range
-    app.get("/api/workouts/range", (req, res) => {
-        Workout.find()
-            .then(dbWorkout => res.json(dbWorkout))
-            .catch(err => {
-                res.error(404).json(err)
-            });
+// range
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find()
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => {
+        res.error(404).json(err)
     });
+});
 
-    app.post("/api/workouts", (req, res) => {
-        Workout.create({})
-            .then(dbWorkout => res.json(dbWorkout))
-            .catch(err => {
-                res.status(404).json(err)
-            });
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(dbWorkout => res.json(dbWorkout))
+    .catch(err => {
+        res.status(404).json(err)
     });
+});
 
-    app.put("/api/workouts/:id", ({ body, params }, res) => {
-        Workout.findByIdAndUpdate(
-            params.id,
-            { $push: { excersises: body } },
-            // 
-            { new: true, runValidators: true }
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { excersises: body } },
+        // 
+        { new: true, runValidators: true }
         )
-            .then(dbWorkout => res.json(dbWorkout))
-            .catch(err => {
-                res.error(404).json(err);
-            });
+        .then(dbWorkout => res.json(dbWorkout))
+        .catch(err => {
+            res.error(404).json(err);
+        });
     });
-}
+    
+    module.exports = router;
--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,14 @@
+var router = require('express').Router();
 var path = require("path");
 
-module.exports = function (app) {
-    app.get("/excersise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    });
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    });
-}
+router.get("/excersise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
