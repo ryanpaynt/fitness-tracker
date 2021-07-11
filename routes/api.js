2e6800c7ce9df710c81fabf46de3bad037615cf7
Ryan <ryanpaynt@uncc.edu>
2021-07-10 23:46:57 -0400

    took out duplicate post method

--- routes/api.js
+++ routes/api.js
@@ -15,14 +15,6 @@ router.get("/api/workouts", (req, res) => {
         res.json(dbWorkout)
     })
     .catch(err => {
-        res.error(404).json(err)
-    });
-});
-
-router.post("/api/workouts", (req, res) => {
-    db.Workout.create(req.body)
-    .then(dbWorkout => res.json(dbWorkout))
-    .catch(err => {
         res.status(404).json(err)
     });
 });
@@ -30,9 +22,19 @@ router.post("/api/workouts", (req, res) => {
 // range
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
-    .then(dbWorkout => res.json(dbWorkout))
+    .then(dbWorkout => {
+
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+        res.json(dbWorkout)
+    })
     .catch(err => {
-        res.error(404).json(err)
+        res.status(404).json(err)
     });
 });
 
@@ -55,7 +57,7 @@ router.put("/api/workouts/:id", (req, res) => {
         { new: true})
         .then(dbWorkout => res.json(dbWorkout))
         .catch(err => {
-            res.error(404).json(err);
+            res.status(404).json(err);
         });
     });
     
