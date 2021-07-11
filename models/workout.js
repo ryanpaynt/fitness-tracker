5e4d2cb78831f98a8866cba9653108861e94c72b
Ryan <ryanpaynt@uncc.edu>
2021-07-10 21:22:41 -0400

    added total duration to workout model

--- models/workout.js
+++ models/workout.js
@@ -36,7 +36,11 @@ const workoutSchema = new Schema({
             type: Number
           }
         }
-      ]
+      ],
+      totalDuration: {
+        type: Number,
+        default: 0,
+      }
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
